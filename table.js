e52e6cdb4b286ab27419ec0de7c9a36facb8635e
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-04-09 14:14:41 +0530

    add program

--- table.js
+++ table.js
@@ -1,4 +1,4 @@
-
+//without return
 function printTable(){
     let num=2;
     for(i=1;i<=10;i++){
@@ -13,3 +13,26 @@ function createTable(num){
     }
 }
 createTable(4);
+
+
+//with return
+
+function table(){
+    let num=6;
+    let temp=[];
+    for(i=1;i<=10;i++){
+        temp.push(`${num}*${i}=${num*i}`);
+    }
+    return temp;
+}
+console.log(table());
+
+
+function findTable(num){
+    let temp=[];
+    for(i=1;i<=10;i++){
+        temp.push(`${num}*${i}=${num*i}`);
+    }
+    return temp;
+}
+console.log(findTable(12));
