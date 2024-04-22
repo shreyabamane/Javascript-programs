8d392aa7198184416a2796a2f42791868d2eeee8
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-04-22 18:46:19 +0530

    print Window content

---
+++ Javascript-Tests/Current-Window-Content.js
@@ -0,0 +1,6 @@
+//Write a JavaScript program to print the current window contents. 
+
+function windowContent(){
+    window.print();// this is the method to print the content of the html elements(window content)
+}
+// export this function and import into the html code
